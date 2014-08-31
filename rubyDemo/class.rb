5151ab630718f75467c259bc85849cdff83e3464
zhaofeng555 <443842006@qq.com>
2014-08-31 23:04:35 +0800

    ruby 类

---
+++ rubyDemo/class.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+class Customer
+  @@no_of_customers=0
+  
+  def initialize(id, name, addr)
+    @cust_id=id
+    @cust_name=name
+    @cust_addr=addr
+  end
+  
+  def display_details()
+    puts "customer id #@cust_id"
+    puts "customer name #@cust_name"
+    puts "customer address  #@cust_addr"
+  end
+  
+  def total_no_of_customers()
+    @@no_of_customers += 1
+    puts "Totals number of customers : #@@no_of_customers"
+  end
+end
+cust1=Customer.new("1", "john", "wisdom apatments")
+cust2=Customer.new("2", "poul", "new empire road")
+
+puts cust1
+puts cust2
+
+cust1.display_details()
+cust2.total_no_of_customers
+cust2.total_no_of_customers
+cust2.total_no_of_customers
